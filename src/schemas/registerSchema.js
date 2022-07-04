785c1e53d9e3b35182768ec339ca062b22af382f
Jhuliana <jholeanasantos@gmail.com>
2022-07-04 06:16:17 -0300

    fix: password

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -1,9 +1,9 @@
 import joi from 'joi';
 
 const registerSchema = joi.object({
-  name: joi.string().required().min(1),
+  name: joi.string().required().min(3),
   email: joi.string().email().required(),
-  password: joi.string().required().min(4),
+  password: joi.string().required().min(8),
   confirmPassword: joi.ref('password')
 });
 
